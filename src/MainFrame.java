61379d1eeeb349f5db4e359739386eff05930a56
mohsin <msis.rubel@gmail.com>
2022-04-26 02:19:10 -0400

    init

---
+++ src/MainFrame.java
@@ -0,0 +1,19 @@
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame {
+    public void name() {
+        setTitle("Hello");
+        setSize(600,400);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public void init() {
+    }
+
+    
+}
+    
+
